1e8b4bb535449acaa9399ec5db0caeaa99125219
Kira <rarakira@gmail.com>
2020-02-12 19:01:31 +0300

    Add toLocaleDateString formatting

--- app.js
+++ app.js
@@ -2,55 +2,26 @@
 
 const express = require("express");
 const ejs = require("ejs");
-// const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-// app.use(express.static("foldername"));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  console.log(today, currentDay);
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  // if (currentDay === 6 || currentDay === 0) {
-  //   day = "Weekend";
-  // } else {
-  //   day = "Weekday";
-  // }
+  var day = today.toLocaleDateString("en-GB", options);
 
-  switch (currentDay) {
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    case 0:
-      day = "Sunday";
-      break;
-    default:
-    console.log("Error, current day is equal to: " + currentDay);
-  }
-  res.render("list", {kindOfDay: day});
+  res.render("list-colour", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
