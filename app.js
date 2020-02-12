2b618d71b97d303c2d6fc66c5fe85eb83e6abc74
Kira <rarakira@gmail.com>
2020-02-12 19:48:57 +0300

    Generate list items from an array. Add new task button

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require("ejs");
 
 const app = express();
 
+var items = ["Buy Food", "Cook food", "Eat food"];
+
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
@@ -21,8 +23,16 @@ app.get("/", function(req, res){
 
   var day = today.toLocaleDateString("en-GB", options);
 
-  res.render("list-colour", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
+
 });
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
 
 app.listen(3000, function(){
   console.log("The server is running on port 3000.");
