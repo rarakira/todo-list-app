2a810a6b23395d576db39f5c95898453899d3da8
Kira <rarakira@gmail.com>
2020-02-13 20:24:11 +0300

    Set up a mongo db

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-
-const items = ["Buy Food", "Cook food", "Eat food"];
-const workItems = [];
 
 const year = date.getYear();
 
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-
-  const day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: items, year: year});
-
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-app.post("/", function(req, res){
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please, add a new task!"]
+  }
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const itemOne = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const itemTwo = new Item({
+  name: "Hit the + Button to add a new item."
+});
+
+const itemThree = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [itemOne, itemTwo, itemThree];
+
+app.get("/", (req, res) => {
+
+  Item.find({}, (err, dbItems) => {
+
+    if (err) {
+      console.log(err);
+    } else {
+
+      if (dbItems.length === 0) {
+        Item.insertMany(defaultItems, (err) => {
+          (err) ? console.log(err): console.log("Successfully added default items!");
+          res.redirect("/");
+        });
+      } else {
+        res.render("list", {listTitle: "Today", newListItems: dbItems, year: year});
+      }
+    }
+  });
+});
+
+app.post("/", function(req, res) {
 
   const item = req.body.newItem;
 
@@ -35,14 +76,20 @@ app.post("/", function(req, res){
 
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", {listTitle: "Work list", newListItems: workItems, year: year});
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work list",
+    newListItems: workItems,
+    year: year
+  });
 });
 
-app.get("/about", function (req, res) {
-  res.render("about", {year: year});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    year: year
+  });
 })
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("The server is running on port 3000.");
 });
