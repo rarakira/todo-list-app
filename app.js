c27ad06f95f3cd33d59cc73d4b083a2ca38a0af5
Kira <rarakira@gmail.com>
2020-02-13 16:53:24 +0300

    Add new list page. Separate header and footer partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const app = express();
 
 let items = ["Buy Food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -24,16 +25,31 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("en-GB", options);
 
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
+
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list === "Work list") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
 
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work list", newListItems: workItems});
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+})
 
 app.listen(3000, function(){
   console.log("The server is running on port 3000.");
